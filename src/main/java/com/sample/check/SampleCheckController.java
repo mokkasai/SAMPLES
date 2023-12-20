0a63abd54555ffc09b788aafc08a77ab1294014c
mokkasai <mokkasaikumargoud@gmail.com>
2023-12-20 22:27:14 +0530

    modified

---
+++ src/main/java/com/sample/check/SampleCheckController.java
@@ -0,0 +1,35 @@
+package com.sample.check;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.print.DocFlavor.STRING;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.xml.ws.WebServiceClient;
+
+@RestController
+public class
+SampleCheckController {
+
+
+@PostMapping("/create")
+
+
+
+
+    public ResponseEntity<Order> createOrder(@RequestBody Order order) {
+
+
+    System.out.println(order.getOrdName()+" "+order.getOrdPrice());
+
+           return ResponseEntity.ok(order);
+    }
+
+
+}
---
+++ src/test/java/com/sample/check/SampleCheckApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sample.check;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleCheckApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
